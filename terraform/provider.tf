a4a9db7492b8f13021a0b59f7db0ecb92eb6893f
Artsiom Kashkou <artsiom.kashkou@jetbrains.com>
2023-02-01 16:23:40 +0100

    Move backend.tf contents to the provider

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_providers {
+    yandex = {
+      source  = "yandex-cloud/yandex"
+      version = "~> 0.84.0"
+    }
+  }
+}
+
+
 provider "yandex" {
   token     = var.yandex_token
   cloud_id  = var.yandex_cloud_id
