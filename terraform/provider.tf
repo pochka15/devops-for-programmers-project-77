3f24575770da35575d82521a02dce526e54ff702
Artsiom Kashkou <artsiom.kashkou@jetbrains.com>
2023-02-08 08:36:59 +0100

    Update yandex cloud provider bring datadog back

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,7 +2,12 @@ terraform {
   required_providers {
     yandex = {
       source  = "yandex-cloud/yandex"
-      version = "~> 0.84.0"
+      version = "~> 0.85.0"
+    }
+
+    datadog = {
+      source = "DataDog/datadog"
+      version = "~> 3.20.0"
     }
   }
 }
@@ -12,4 +17,10 @@ provider "yandex" {
   cloud_id  = var.yandex_cloud_id
   folder_id = var.yandex_folder_id
   zone      = "ru-central1-a"
+}
+
+provider "datadog" {
+  api_key = var.datadog_api_key
+  app_key = var.datadog_app_key
+  api_url = "https://api.datadoghq.eu/"
 }
