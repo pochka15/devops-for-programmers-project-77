97658447cecd206c55f41c3b8a40a87e1beb677a
Artsiom Kashkou <artsiom.kashkou@jetbrains.com>
2023-02-08 08:30:23 +0100

    Tmp Remove datadog from providers

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -4,11 +4,6 @@ terraform {
       source  = "yandex-cloud/yandex"
       version = "~> 0.84.0"
     }
-
-    datadog = {
-      source = "DataDog/datadog"
-      version = "~> 3.20.0"
-    }
   }
 }
 
@@ -17,10 +12,4 @@ provider "yandex" {
   cloud_id  = var.yandex_cloud_id
   folder_id = var.yandex_folder_id
   zone      = "ru-central1-a"
-}
-
-provider "datadog" {
-  api_key = var.datadog_api_key
-  app_key = var.datadog_app_key
-  api_url = "https://api.datadoghq.eu/"
 }
